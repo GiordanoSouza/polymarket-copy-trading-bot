c2ec80fc1877cb34dee9147c525dddd1041c3557
Giordano de Pinho Souza <giordanowrk@gmail.com>
2025-10-25 08:49:56 -0300

    yaml

---
+++ scripts/config_constrains.py
@@ -0,0 +1,8 @@
+
+
+def config_constrains(asset: str, side: str, price: float, usdc_size: float, type: str):
+    
+    # Getting the active positions of the player assets
+    
+    # Constrain one: percentage
+    
